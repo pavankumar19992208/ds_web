b5314f655a37dfafe430342a4c7575589f28a9b6
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-06-26 12:19:01 +0530

    popup decorated -siri

--- my-app/src/components/popups/LoginPopup.js
+++ my-app/src/components/popups/LoginPopup.js
@@ -4,52 +4,33 @@ import './LoginPopup.css'; // Assuming you will create this CSS file
 
 const LoginPopup = ({ onClose }) => {
   return (
-    /*<div className="loginPopup">
-      <div className="loginPopupContainer">
-        <div className="loginHeader">
-          <h2>ADMINISTRATION LOGIN</h2>
-          <button onClick={onClose} className="closeButton">X</button>
-        </div>
-        <div className="loginBody">
-          <div className="inputGroup">
-            <label>USER ID</label>
-            <input type="text" placeholder="XXXXXXXXXX" />
-          </div>
-          <div className="inputGroup">
-            <label>PASSWORD</label>
-            <input type="password" placeholder="XXXXXXXXXX" />
-          </div>
-          <button className="loginButton">LOGIN</button>
-          <button className="forgotPassword">forgot password</button>
-        </div>
-      </div>
-    </div>*/
-
 <div className="loginPopup">
   <div className="loginPopupContainer">
+    <div className='widget-container'>
     <div className="loginHeader">
-      <h2>ADMINISTRATION  LOGIN</h2>
-      <button onClick={onClose} className="closeButton">X</button>
+      <h2 style={{marginLeft:'10%'}}>ADMINISTRATION  LOGIN</h2>
+      <button onClick={onClose} className="closeButton widget-align-right">X</button>
     </div>
     <div className="loginBody">
       <div className="inputGroup inputIcon">
-        <h4 className='space'>USER ID</h4>
-        <i className="fas fa-user "></i>
+        <h4 className='space widget-align-left'>USER ID</h4>
+        <i className="fas fa-user widget-align-left"></i>
         <input type="text" placeholder="" />
       </div>
       <div className="inputGroup inputIcon">
-        <h4 className='space'>PASSWORD</h4>
-        <i className="fas fa-lock"></i>
+        <h4 className='space widget-align-left'>PASSWORD</h4>
+        <i className="fas fa-lock widget-align-left"></i>
         <input type="password" placeholder="" />
       </div>
       <div class="button-grid">
-      <button className="forgotPassword">forgot password</button>
-      <button className="loginButton">LOGIN</button>
+      <button className="forgotPassword widget-align-left" >forgot password</button>
+      <button className="loginButton widget-align-right">LOGIN</button>
       </div>
     </div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   </div>
 </div> 
+</div>
   );
 };
 
