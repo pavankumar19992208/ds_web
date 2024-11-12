00b74fe9fd6d62995e381dd7c64b00debf402ee7
pavankumar19992208 <pavankumar19992208@gmail.com>
2024-11-12 13:41:16 +0530

    --pr

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 // Define the base URL for the backend
-const BaseUrl = 'https://0e20-2406-b400-d1-ea60-3cac-edfe-acc4-3d62.ngrok-free.app';
+const BaseUrl = 'https://680a-223-196-193-20.ngrok-free.app';
 
 // Export the base URL so it can be used in other files
 export default BaseUrl;
