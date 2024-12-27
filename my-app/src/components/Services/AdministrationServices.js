cb191a30885416653848d689b8e7256ce0832f06
j jeffi <jeffi7426@gmail.com>
2024-12-27 20:11:57 +0530

    change project heiorarchy

--- my-app/src/components/Services/AdministrationServices.js
+++
@@ -1,7 +0,0 @@
-export default function AdministrationServices(props) {
-  return (
-    <div style={{}}>
-     <p style={{ margin: 0 }}>klghcfhjbdncvkdnvkljbhdv</p>
-    </div>
-  );
-}
---
+++ src/components/Services/AdministrationServices.js
@@ -0,0 +1,7 @@
+export default function AdministrationServices(props) {
+  return (
+    <div style={{}}>
+     <p style={{ margin: 0 }}>klghcfhjbdncvkdnvkljbhdv</p>
+    </div>
+  );
+}
